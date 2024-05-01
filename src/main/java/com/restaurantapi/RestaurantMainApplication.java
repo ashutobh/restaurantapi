c243217455db0d1513a846647fc04297b6dd10a9
Ashutosh Bhardwaj <ashutosh0313@gmail.com>
2024-05-01 15:55:51 +0800

    updated code

--- src/main/java/com/restaurantapi/RestaurantMainApplication.java
+++ src/main/java/com/restaurantapi/RestaurantMainApplication.java
@@ -1,20 +1,9 @@
 package com.restaurantapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan({"com.restaurantapi.controller", "com.restaurantapi.service"})
-@EntityScan("com.restaurantapi.entity")
-@EnableJpaRepositories("com.restaurantapi.repository")
+@SpringBootApplication
 public class RestaurantMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestaurantMainApplication.class, args);
